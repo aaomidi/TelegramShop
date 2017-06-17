817b241f56575a13888d04fee2f9a8c65d713546
Amir Omidi <amir@aaomidi.com>
2017-06-16 20:49:06 -0400

    2 space indentation.

--- model/shopItem.js
+++ model/shopItem.js
@@ -1,45 +1,45 @@
 class ShopItem {
-    constructor(shop, name, description, duration, cost) {
-        this.shop = shop;
-        this.name = name;
-        this.description = description;
-        this.duration = duration;
-        this.cost = cost;
-    }
+  constructor(shop, name, description, duration, cost) {
+    this.shop = shop;
+    this.name = name;
+    this.description = description;
+    this.duration = duration;
+    this.cost = cost;
+  }
 
-    getShop() {
-        return this.shop;
-    }
+  getShop() {
+    return this.shop;
+  }
 
-    getName() {
-        return this.name;
-    }
+  getName() {
+    return this.name;
+  }
 
-    setName(name) {
-        this.name = name;
-    }
+  setName(name) {
+    this.name = name;
+  }
 
-    getDescription() {
-        return this.description;
-    }
+  getDescription() {
+    return this.description;
+  }
 
-    setDescription(description) {
-        this.description = description;
-    }
+  setDescription(description) {
+    this.description = description;
+  }
 
-    getDuration() {
-        return this.duration;
-    }
+  getDuration() {
+    return this.duration;
+  }
 
-    setDuration(duration) {
-        this.duration = duration;
-    }
+  setDuration(duration) {
+    this.duration = duration;
+  }
 
-    getCost() {
-        return this.cost;
-    }
+  getCost() {
+    return this.cost;
+  }
 
-    setCost(cost) {
-        this.cost = cost;
-    }
+  setCost(cost) {
+    this.cost = cost;
+  }
 }
